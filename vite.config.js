f0ff3e3d45a3a21abe66af1ef3e6677a49069904
Timothy Embden <229timmy@gmail.com>
2025-01-14 01:43:46 -0500

    fix: update build configuration and dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,33 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
+  base: '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html')
+      },
       output: {
-        manualChunks: undefined
+        manualChunks: undefined,
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name.split('.').at(1);
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = 'img';
+          }
+          return `assets/${extType}/[name]-[hash][extname]`;
+        },
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
       }
     }
   },
   server: {
     port: 3000
+  },
+  css: {
+    devSourcemap: true
   }
 }); 
